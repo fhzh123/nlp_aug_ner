baa1e485226a5ee75e1835633ebd79a6a982c6bf
fhzh123 <fhzh@naver.com>
2021-05-27 01:36:26 +0900

    preprocessing refactoring...

--- preprocessing.py
+++ preprocessing.py
@@ -19,9 +19,51 @@ def preprocessing(args):
     #=============Data Load=============#
     #===================================#
 
-    # 1) Comment data open
-    train = pd.read_csv(os.path.join(args.data_path, 'train.csv'))
-    test = pd.read_csv(os.path.join(args.data_path, 'test.csv'))
+    dataset_dict = dict()
+
+    # 1-1) IMDB data open
+    dataset_dict['imdb'] = {
+        'train': pd.read_csv(os.path.join(args.imdb_data_path, 'train.csv'),
+            names=['label', 'comment']).replace({
+                'sentiment': {'positive': 0, 'negative': 1}
+            }, inplace=True),
+        'test': pd.read_csv(os.path.join(args.imdb_data_path, 'test.csv'),
+            names=['label', 'comment']).replace({
+                'sentiment': {'positive': 0, 'negative': 1}
+            }, inplace=True))
+    }
+
+    # 1-2) Yelp data open
+    dataset_dict['yelp'] = {
+        'train': pd.read_csv(os.path.join(args.yelp_data_path, 'train.csv'), 
+            names=['label', 'comment']),
+        'test': pd.read_csv(os.path.join(args.yelp_data_path, 'test.csv'), 
+            names=['label', 'comment'])
+    }
+
+    # 1-3) Yahoo data open
+    dataset_dict['yahoo'] = {
+        'train': pd.read_csv(os.path.join(args.yahoo_data_path, 'train.csv'), 
+            names=['label', 'question_title', 'question_content', 'best_answer']),
+        'test': pd.read_csv(os.path.join(args.yahoo_data_path, 'test.csv'), 
+            names=['label', 'question_title', 'question_content', 'best_answer']),
+    }
+
+    # 1-4) AG News data open
+    dataset_dict['ag_news'] = {
+        'train': pd.read_csv(os.path.join(args.ag_news_data_path, 'train.csv'), 
+            names=['label', 'title', 'description']),
+        'test': pd.read_csv(os.path.join(args.ag_news_data_path, 'test.csv'), 
+            names=['label', 'title', 'description'])
+    }
+
+    # 1-5) DBpedia data open
+    dataset_dict['dbpedia'] = {
+        'train': pd.read_csv(os.path.join(args.dbpedia_data_path, 'train.csv'), 
+            names=['label', 'title', 'description']),
+        'test': pd.read_csv(os.path.join(args.dbpedia_data_path, 'test.csv'), 
+            names=['label', 'title', 'description'])
+    }
 
     # 2) Path setting
     if not os.path.exists(args.preprocess_path):
@@ -37,22 +79,13 @@ def preprocessing(args):
     tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
 
     #===================================#
-    #=============Cleansing=============#
+    #=============Encoding==============#
     #===================================#
 
-    print('Cleansing...')
-
-    train['comment'] = encoding_text(train['comment'], tokenizer, args.max_len)
-    test['comment'] = encoding_text(test['comment'], tokenizer, args.max_len)
-
-    #===================================#
-    #==========Label processing=========#
-    #===================================#
-
-    print('Label processing...')
-
-    train.replace({'sentiment': {'positive': 0, 'negative': 1}}, inplace=True)
-    test.replace({'sentiment': {'positive': 0, 'negative': 1}}, inplace=True)
+    print('Encoding...')
+    for data in ['imdb', 'yelp', 'yahoo', 'ag_news', 'dbpedia']:
+        train['comment'] = encoding_text(train['comment'], tokenizer, args.max_len)
+        test['comment'] = encoding_text(test['comment'], tokenizer, args.max_len)
 
     #===================================#
     #==============Saving===============#
